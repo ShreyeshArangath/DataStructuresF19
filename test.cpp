4b9a73e7f5c0154278a2ce555c2cdc69a28395c8
Shreyesh <imshreyesh17@gmail.com>
2019-09-12 18:13:35 -0500

    delete from beginning added

--- test.cpp
+++ test.cpp
@@ -16,7 +16,7 @@ linked_list_node_t* createNewNode(float data){
 
 linked_list_node_t* insertAtBeginning(float data, linked_list_node_t **head,linked_list_node_t **tail){
     linked_list_node_t *temp = createNewNode(data);
-    if (*head == NULL and *tail==NULL){
+    if (*head == NULL && *tail==NULL){
         *head=temp;
         *tail=temp;
         (*head)->next=NULL;
@@ -30,7 +30,7 @@ linked_list_node_t* insertAtBeginning(float data, linked_list_node_t **head,link
 
 linked_list_node_t* insertAtTheEnd(float data, linked_list_node_t**head, linked_list_node_t **tail){
     linked_list_node_t *temp = createNewNode(data);
-    if (*tail==NULL and *head==NULL){
+    if (*tail==NULL && *head==NULL){
         *head=temp;
         *tail=temp;
         (*tail)->next=NULL;
@@ -43,8 +43,50 @@ linked_list_node_t* insertAtTheEnd(float data, linked_list_node_t**head, linked_
     return *head;
 }
 
-linked_list_node_t* insertAfterTheValue(float dataToInsert, float dataToSearch, linked_list_node_t** head, linked_list_node_t **ta){
+linked_list_node_t* insertAfterTheValue(float dataToInsert, float dataToSearch, linked_list_node_t** head, linked_list_node_t **tail){
+    
+    linked_list_node_t *cur = *head;
+    while(cur!=NULL && cur->data!=dataToSearch){
+        cur=cur->next;
+    }
 
+    if(cur==NULL){
+        std::cout<<"No specified records exist. Insertion is not possible. \n";
+    }
+    else{
+        if (cur==*head){
+            insertAtBeginning(dataToInsert,head,tail);
+        }
+        else if(cur==*tail){
+            insertAtTheEnd(dataToInsert,head,tail);
+        }
+        else {
+            linked_list_node_t *new_node = createNewNode(dataToInsert);
+            linked_list_node_t *temp=cur->next;
+            cur->next=new_node;
+            new_node->next=temp;
+        }
+
+    }
+    return *head;
+}
+
+
+linked_list_node_t * deleteAtTheBeginning(linked_list_node_t **head, linked_list_node_t **tail) {
+    if (*head==NULL){
+        std::cout<<"No records exist in the list. Deletion not possible.\n";
+    }
+    else{
+        if ((*head)->next == NULL){
+            *head=*tail=NULL;
+            return *head;
+        }
+        linked_list_node_t *temp = *head;
+        *head=(*head)->next;
+        free(temp);
+
+    }
+    return *head;
 }
 
 void printFunction(linked_list_node_t **head){
@@ -58,10 +100,19 @@ void printFunction(linked_list_node_t **head){
 int main(){
     linked_list_node_t *head = NULL;
     linked_list_node_t *tail = NULL;
-    insertAtBeginning(1.2,&head,&tail);
-    insertAtBeginning(2.4,&head,&tail);
-    insertAtTheEnd(3.2,&head,&tail);
-    insertAtBeginning(3.3,&head,&tail);
+    head=insertAtBeginning(1.2,&head,&tail);
+    head=insertAtBeginning(2.4,&head,&tail);
+    head=insertAtTheEnd(3.2,&head,&tail);
+    head=insertAtBeginning(3.3,&head,&tail);
+    printFunction(&head);
+    std::cout<<"\n";
+    head=insertAfterTheValue(1.6,2.4,&head,&tail);
+    printFunction(&head);
+    deleteAtTheBeginning(&head,&tail);
+    deleteAtTheBeginning(&head,&tail);
+    deleteAtTheBeginning(&head,&tail);
+    deleteAtTheBeginning(&head,&tail);
+    std::cout<<"\n";
     printFunction(&head);
     return 0;
 }
