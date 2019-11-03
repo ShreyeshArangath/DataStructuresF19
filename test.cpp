51bd653a0d515883e129c381830f7ec944cda215
Shreyesh <imshreyesh17@gmail.com>
2019-11-02 20:11:46 -0500

    stack functions added

--- test.cpp
+++ test.cpp
@@ -23,27 +23,51 @@ tree_node_t* createNewTreeNode(char data) {
     return temp;
 }
 
-void reverseArray(int arr[], int start, int end) 
-{ 
-    while (start < end){
-        int temp = arr[start];  
-        arr[start] = arr[end]; 
-        arr[end] = temp; 
-        start++; 
-        end--; 
-    }  
-}      
+stack_node_t* createStackNode(tree_node_t* data){
+    stack_node_t* temp = (stack_node_t*)malloc(sizeof(stack_node_t));
+    temp->data=data;
+    return temp;
+}
+
+stack_node_t* PUSH(tree_node_t* data, stack_node_t** top){
+    stack_node_t* temp = createStackNode(data);
+    temp->next=*top;
+    *top=temp;
+    return *top;
+}
+
+tree_node_t* POP(stack_node_t **top){
+    if(*top==NULL){
+        cout<<"Cannot pop from an empty stack";
+        return NULL;
+    }
+    stack_node_t* temp = *top;
+    tree_node_t* data = temp->data;
+    (*top)=(*top)->next;
+    free(temp);
+    return data;
+}
+
 
 int isSymbol(char element){
     return 1 ? (element=='+' || element=='-' || element=='*' || element=='/' || element=='^' || element =='%') : 0;
 }
 
 void changeToExpressionTree(char* expression){
-    stack_node_t* temp_stack;
-    tree_node_t* tree, temp_tree_1, temp_tree_2;
+    stack_node_t* temp_stack_top;
+    tree_node_t* tree, *temp_tree_1, *temp_tree_2;
     for(int i=0; i<strlen(expression); i++){
         if(isSymbol(expression[i])){
-            tree = 
+            tree = createNewTreeNode(expression[i]);
+            temp_tree_1=temp_stack_top->data;
+            POP(&temp_stack_top);
+            temp_tree_2=temp_stack_top->data;
+            POP(&temp_stack_top);
+
+            tree->right=temp_tree_1;
+            tree->left= temp_tree_2;
+
+        
         }
     }
 }
