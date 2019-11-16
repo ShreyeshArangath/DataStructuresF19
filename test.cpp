f84dd98a933df35cd8119ea6452af8758c452804
Shreyesh <imshreyesh17@gmail.com>
2019-11-16 16:26:56 -0600

    shortest path function initialized

--- test.cpp
+++ test.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 typedef struct VerticeNode{
+    char vertice;
     int weight;
 }graph_vertice_t;
 
@@ -17,7 +18,9 @@ int getIndex(char val){
 
 int createNewEdge(graph_vertice_t graph[][MAX], char vertice, char connectedTo, int weight){
     graph[getIndex(vertice)][getIndex(connectedTo)].weight = weight;
+    graph[getIndex(vertice)][getIndex(connectedTo)].vertice=connectedTo;
     graph[getIndex(connectedTo)][getIndex(vertice)].weight=weight;
+    graph[getIndex(connectedTo)][getIndex(vertice)].vertice=vertice;
     return TRUE;
 }
 
@@ -69,6 +72,36 @@ void Display(graph_vertice_t graph[][MAX]){
             print_vertice_and_edge(graph, i);
         }
     }
+}
+
+void initialize_algorithm(int set_of_vertices[], int distance_from_source[], int source){
+    for(int i=0 ; i<MAX ; i++){
+        set_of_vertices[i]=FALSE;
+        distance_from_source[i]=INT_MAX;
+    }
+    distance_from_source[source]=0;
+}
+
+int findMinimumDistance(int set_of_vertices[], int distance_from_source[]){
+    int minimum_val = INT_MAX;
+    int minimum_index;
+    for(int i=0; i<MAX; i++){
+        if(distance_from_source[i]<=minimum_val && set_of_vertices[i]==false){
+            minimum_val=distance_from_source[i];
+            minimum_index=i;
+
+        }
+    }
+    return minimum_index;
+}
+
+void Shortest_path(graph_vertice_t graph[][MAX], int source){
+    int set_of_vertices[MAX];
+    int distance_from_source[MAX];
+
+    initialize_algorithm(set_of_vertices, distance_from_source, source);
+
+
 }
 
 int main(){
